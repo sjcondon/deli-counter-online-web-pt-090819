b455e0823194a6a2cf72abed2e678e223fbd98ef
sarah <s.j.condon01@gmail.com>
2019-09-18 02:22:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = [ ]
+count = 1
+
+if count < 1 
+  puts "The line is currently empty"
+  count =+ 1
+
+def take_a_number(katz_deli, name)
+  puts "#{name} you are #{katz_deli}"
+
+
+  
