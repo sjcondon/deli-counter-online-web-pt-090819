5c6451f26140960da6486dd7bd412757c5d5b595
sarah <s.j.condon01@gmail.com>
2019-09-25 02:08:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,21 +2,22 @@ def line(katz_deli)
  names= "The line is currently:"
  
  
- if katz_deli.empty?
-  puts "The line is currently empty."
- else
-   katz_deli.each_with_index do |name,index|
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name,index|
      names <<" #{index+1}. #{name}"
    end
 puts names
+    end
  end
  def now_serving
    if now_serving > 1
-   puts "Welcome, #{name}. You are number #{index+1} in line"
-else now_serving.empty?
-  puts "There is nobody waiting to be served!"
-end
-end
-end
+      puts "Welcome, #{name}. You are number #{index+1} in line"
+  else now_serving.empty?
+      puts "There is nobody waiting to be served!"
+      end
+    end
+
 
   
